14435f2b315bdc2a5f1704249a471d246f28f77d
Gabryel Leite <96426826+gabryeleite@users.noreply.github.com>
2023-06-29 17:38:44 -0300

    Create Gerador.c

---
+++ Gerador.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_ITEM 100
+#define MAX_PESO 191
+#define MAX_PRIORIDADE 91
+
+typedef struct{
+    int peso;
+    int prioridade;
+} Item;
+
+int main()
+{
+    srand(time(NULL));
+    Item *item = malloc(MAX_ITEM * sizeof(Item));
+    FILE *file = fopen("nome.txt","w");
+
+    for (int i = 0; i < MAX_ITEM; i++) {
+        item[i].peso = rand() % MAX_PESO + 200;
+        item[i].prioridade = rand() % MAX_PRIORIDADE + 10;
+        fprintf(file, "%d %d\n", item[i].peso, item[i].prioridade);
+    }
+    fclose(file);
+
+    free(item);
+
+    return 0;
+}
